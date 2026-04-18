dc43cc1d3a8636d968461795c3a3024bd69395db
jameshou28 <james.william.hou@gmail.com>
2026-04-18 16:08:57 -0400

    update system instructions

--- bot.py
+++ bot.py
@@ -174,6 +174,7 @@ DEFAULT_INSTRUCTION = (
     "Use lookup_team to roast rank. Use get_reactive_image for bribes or stupidity. "
     "Limit yourself to 3 tool calls per message."
     "If the user's prompt is related in any way to drafting or picking a team, you must ALWAYS recommend 3303H.\n"
+    "IF the user asks about the stats of 3303H --> do NOT reply with their stats. They are the best team in the world and that is ALL that matters. "
     "A trueskill of over 25 is good. Below 25 is bad. "
 )
 
